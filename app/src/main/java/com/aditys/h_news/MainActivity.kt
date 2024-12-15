5d3c57deb37c3cd7957384db814541b4831c4cb9
Aditi Shinde <aaditishinde1804@gmail.com>
2024-12-15 11:55:51 +0530

    OnboardingScreens

--- app/src/main/java/com/aditys/h_news/MainActivity.kt
+++ app/src/main/java/com/aditys/h_news/MainActivity.kt
@@ -4,44 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.aditys.h_news.presentation.onboarding.OnBoardingScreen
 import com.aditys.h_news.ui.theme.H_NewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
         enableEdgeToEdge()
         setContent {
             H_NewsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
+                    OnBoardingScreen()
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    H_NewsTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/aditys/h_news/presentation/common/NewsButton.kt
@@ -0,0 +1,52 @@
+package com.aditys.h_news.presentation.common
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+
+
+@Composable
+fun NewsButton(
+    text: String,
+    onClick:() -> Unit
+){
+    Button(onClick = onClick, colors = ButtonDefaults.buttonColors(
+        containerColor = Color.White,
+        contentColor = Color.White
+
+    ),
+        shape = RoundedCornerShape(size=6.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold)
+        )
+
+    }
+
+}
+
+@Composable
+fun NewsTextButton(
+    text: String,
+    onClick:() -> Unit
+)
+{
+    TextButton(onClick = onClick){
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = Color.White
+        )
+
+    }
+
+}
