bd112347307c3cdf36d8124e3fd249645cbfcedd
Aditi Shinde <aditis.github@gmail.com>
2025-07-08 17:05:20 +0530

    Ft:ProfileScreen

--- app/src/main/java/com/aditys/h_news/view/pages/BookmarksPage.kt
+++
@@ -1,9 +0,0 @@
-package com.aditys.h_news.view.pages
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun BookmarksPage() {
-    Text(text = "Bookmarks Page")
-}
