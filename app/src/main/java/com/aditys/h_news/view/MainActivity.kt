dc8de5f97be768f1c8920ddf8b3271251cd1edf7
Aditi Shinde <aditis.github@gmail.com>
2025-07-10 18:10:55 +0530

    Fix:AppNavigation

--- app/src/main/java/com/aditys/h_news/view/MainActivity.kt
+++ app/src/main/java/com/aditys/h_news/view/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
+import android.os.Build
+import android.view.WindowInsetsController
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowInsetsControllerCompat
 import com.aditys.h_news.theme.H_NewsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Make content draw behind the system bars
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Set status bar transparent
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
+
+        // Optional: Set light or dark icons for the status bar
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = false // false = white icons
+
         setContent {
             H_NewsTheme {
                 AppNavigation()
