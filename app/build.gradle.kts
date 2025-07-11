0aac28eedef94a3cd7a24e8ee15f39935633388c
anas shikoh <anas.shikoh@kukufm.com>
2025-07-11 21:01:34 +0530

    removed some duplicate dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,28 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.core.splashscreen) //Splash Api
+    implementation(libs.retrofit) //Retrofit
+    implementation(libs.converter.gson)
+    implementation(libs.coil.compose) //Coil
+    implementation(libs.androidx.datastore.preferences) //Datastore
+    implementation(libs.androidx.foundation) //Compose Foundation
+    implementation(libs.accompanist.systemuicontroller) //Accompanist
+    implementation(libs.paging.runtime) //Paging 3
+    implementation(libs.androidx.paging.compose)
+    implementation(libs.hilt.android) //hilt
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.room.room.runtime) //Room
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.navigation.compose) //Compose Navigation
+    implementation(libs.kotlinx.coroutines.android)  // Coroutines for asynchronous programming
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.navigation.navigation.compose)
+    implementation(libs.ui)
+    implementation(libs.ui.tooling)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -59,31 +81,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
-    implementation (libs.androidx.core.splashscreen) //Splash Api
-    implementation( libs.retrofit) //Retrofit
-    implementation(libs.converter.gson)
-    implementation(libs.coil.compose) //Coil
-    implementation( libs.androidx.datastore.preferences) //Datastore
-    implementation (libs.androidx.foundation) //Compose Foundation
-    implementation (libs.accompanist.systemuicontroller) //Accompanist
-    implementation (libs.paging.runtime) //Paging 3
-    implementation( libs.androidx.paging.compose)
-    implementation(libs.hilt.android) //hilt
-    kapt(libs.hilt.android.compiler)
-    implementation (libs.androidx.hilt.navigation.compose.v100)
-    implementation (libs.androidx.room.room.runtime) //Room
-    kapt ("androidx.room:room-compiler:2.6.1")
-    implementation (libs.androidx.room.ktx.v252)
-    implementation (libs.navigation.compose) //Compose Navigation
-    implementation (libs.kotlinx.coroutines.android)  // Coroutines for asynchronous programming
-
-    implementation(libs.androidx.material)
-    implementation(libs.androidx.navigation.navigation.compose)
-    implementation(libs.ui)
-    implementation(libs.ui.tooling)
-    implementation(libs.androidx.runtime.livedata.v105)
-
-    
-
 }
